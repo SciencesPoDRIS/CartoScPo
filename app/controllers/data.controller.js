cec4f40f85e07ca294fcd2d47a2ba5bcd1666bb6
gtible <gregory.tible@sciencespo.fr>
2016-07-07 17:24:11 +0200

    add spreadsheet and export of data

--- app/controllers/data.controller.js
+++ app/controllers/data.controller.js
@@ -3,40 +3,57 @@
 /* Controllers */
 
 angular.module('bib.controller.data', [])
-  .controller('data', [ '$scope', '$location', "$http", "fileService", 
-    function($scope, $location, $http, fileService) {
+  .controller('data', [ '$scope', '$location', "$http", "fileService", "_", "uiGridConstants", 
+    function($scope, $location, $http, fileService, _, uiGridConstants) {
 
     var url = '../data/Donnees_centres_de_recherche_SP_2015 - Description administrative.csv'
    // get csv from url
    fileService
      .getFile(url)
      .then(function (result) {
-      console.log('result', result);
-     })
+      //transform csv to json
+      var parsed = Papa.parse(result, {header: true, encoding: "utf-8"});
 
-   $scope.myData = [
-    {
-        'firstName': 'Cox',
-        'lastName': 'Carney',
-        'company': 'Enormo',
-        'employed': true
-    },
-    {
-        'firstName': 'Lorraine',
-        'lastName': 'Wise',
-        'company': 'Comveyer',
-        'employed': false
-    },
-    {
-        'firstName': 'Nancy',
-        'lastName': 'Waters',
-        'company': 'Fuelton',
-        'employed': false
-    }
-];
+      // select only five colomns in csv
+      var data = []
+      _.forEach(parsed.data, function(v, k) {
+        var center = {};
 
-   // setup grid with option
+        center['Intitulé'] = v['Intitulé'];
+        center['Sigle ou acronyme'] = v['Sigle ou acronyme'];
+        center['Ville'] = v['Ville'];
+        center['Code Unité'] = v['Code Unité'];
 
-   // export data
+        data.push(center);
+      })
 
+      $scope.myData = data;
+      $scope.gridOptions.data = $scope.myData;
+    });
+
+      //grid settings
+      $scope.gridOptions = {
+        enableFiltering: true,
+        columnDefs: [
+          { field: 'Intitulé', enableFiltering: true},
+          { field: 'Sigle ou acronyme', enableFiltering: true},
+          { field: 'Ville' , enableFiltering: true},
+          { field: 'Code Unité' , enableFiltering: true}
+        ]
+      };
+
+      // export data as csv
+      $scope.exportData = function() {
+        var csv = Papa.unparse($scope.myData),
+            blob = new Blob([csv], {type: 'attachment/csv;charset=utf-8'}),
+            dataUrl = URL.createObjectURL(blob);
+
+        var a = document.createElement('a');
+        a.style.display = 'none';
+        a.setAttribute('href', dataUrl);
+        document.body.appendChild(a);
+        a.setAttribute('download', 'data' + '.csv');
+        a.click();
+        document.body.removeChild(a);
+      }
   }])
