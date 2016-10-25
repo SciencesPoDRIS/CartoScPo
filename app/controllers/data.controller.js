1b34d11c204306899e0cd49d13527203e1039174
Anne L'Hôte <anne.lhote@gmail.com>
2016-10-25 15:56:23 +0200

    Reindent the data controller

--- app/controllers/data.controller.js
+++ app/controllers/data.controller.js
@@ -3,91 +3,92 @@
 /* Controllers */
 
 angular.module('bib.controller.data', [])
-  .controller('data', [ '$scope', '$location', "$http", "fileService", "_", "uiGridConstants", "$filter",
-    function($scope, $location, $http, fileService, _, uiGridConstants, $filter) {
+    .controller('data', ['$scope', '$location', "$http", "fileService", "_", "uiGridConstants", "$filter",
+        function($scope, $location, $http, fileService, _, uiGridConstants, $filter) {
 
-    localStorage.setItem("loadingPage",false);
-   
-   var url = './data/data.json'
-   // get csv from url
-    
+            localStorage.setItem("loadingPage", false);
 
-    //grid settings
-    $scope.gridOptions = {
-      enableFiltering: true,
-      // enableHighlighting: true,
-      // enableGridMenu: true,
-      // exporterMenuPdf: false,
-      onRegisterApi: function(gridApi) {
-        $scope.gridApi = gridApi;
-      },
-      columnDefs: [
-        { field: 'Intitulé', enableFiltering: true},
-        { field: 'Sigle ou acronyme', enableFiltering: true},
-        { field: 'Ville' , enableFiltering: true},
-        { field: 'Code Unité' , enableFiltering: true}
-      ]
-    };
+            var url = './data/data.json'
+                // get csv from url
 
-    $scope.filterText;
-    // full text search -> see app.js for the filter
-    $scope.refreshData = function() {
-      $scope.gridOptions.data = $filter('filter')($scope.myData, $scope.filterText, undefined);
-   };
 
-    // // export data as csv
-    $scope.exportData = function() {
-     
-      var dataSelected = []
-       _.forEach($scope.gridApi.grid.renderContainers.body.visibleRowCache, function(d) {
-          delete d.entity["id"];
-          delete d.entity["adressesGeo"];
-          delete d.entity["theme"];
-          delete d.entity["$$hashKey"];
-          delete d.entity["Commentaires"];
-          delete d.entity["Logo"];
-          dataSelected.push(d.entity);
-       })
+            //grid settings
+            $scope.gridOptions = {
+                enableFiltering: true,
+                // enableHighlighting: true,
+                // enableGridMenu: true,
+                // exporterMenuPdf: false,
+                onRegisterApi: function(gridApi) {
+                    $scope.gridApi = gridApi;
+                },
+                columnDefs: [
+                    { field: 'Intitulé', enableFiltering: true },
+                    { field: 'Sigle ou acronyme', enableFiltering: true },
+                    { field: 'Ville', enableFiltering: true },
+                    { field: 'Code Unité', enableFiltering: true }
+                ]
+            };
 
-      var csv = Papa.unparse(dataSelected),
-          blob = new Blob([csv], {type: 'attachment/csv;charset=utf-8'}),
-          dataUrl = URL.createObjectURL(blob);
+            $scope.filterText;
+            // full text search -> see app.js for the filter
+            $scope.refreshData = function() {
+                $scope.gridOptions.data = $filter('filter')($scope.myData, $scope.filterText, undefined);
+            };
 
-      var a = document.createElement('a');
-      a.style.display = 'none';
-      a.setAttribute('href', dataUrl);
-      document.body.appendChild(a);
-      a.setAttribute('download', 'data' + '.csv');
-      a.click();
-      document.body.removeChild(a);
-    }
+            // // export data as csv
+            $scope.exportData = function() {
 
-    $scope.resetSearch = function() {
-      $scope.filterText = '';
-      $scope.gridOptions.data = $filter('filter')($scope.myData, $scope.filterText, undefined);
-    }
+                var dataSelected = []
+                _.forEach($scope.gridApi.grid.renderContainers.body.visibleRowCache, function(d) {
+                    delete d.entity["id"];
+                    delete d.entity["adressesGeo"];
+                    delete d.entity["theme"];
+                    delete d.entity["$$hashKey"];
+                    delete d.entity["Commentaires"];
+                    delete d.entity["Logo"];
+                    dataSelected.push(d.entity);
+                })
 
-    fileService
-     .getFile(url)
-     .then(function (result) {
+                var csv = Papa.unparse(dataSelected),
+                    blob = new Blob([csv], { type: 'attachment/csv;charset=utf-8' }),
+                    dataUrl = URL.createObjectURL(blob);
 
-      // select only five colomns in csv
-      var data = [],
-          headers = [];
-      _.forEach(result.allCenters, function(tab, k) {
-        var center = {};
-        _.forEach(tab, function(onglet, k) {
-          _.forEach(onglet, function(content, prop) {
-            if (prop !== "theme" && prop !== "id") {
-              center[prop] = content;
-              headers.push({field: prop, enableFiltering: true});
+                var a = document.createElement('a');
+                a.style.display = 'none';
+                a.setAttribute('href', dataUrl);
+                document.body.appendChild(a);
+                a.setAttribute('download', 'data' + '.csv');
+                a.click();
+                document.body.removeChild(a);
             }
-          });
-        });
-        data.push(center);
-      });
 
-      $scope.myData = data;
-      $scope.gridOptions.data = $scope.myData;
-    });
-}])
+            $scope.resetSearch = function() {
+                $scope.filterText = '';
+                $scope.gridOptions.data = $filter('filter')($scope.myData, $scope.filterText, undefined);
+            }
+
+            fileService
+                .getFile(url)
+                .then(function(result) {
+
+                    // select only five colomns in csv
+                    var data = [],
+                        headers = [];
+                    _.forEach(result.allCenters, function(tab, k) {
+                        var center = {};
+                        _.forEach(tab, function(onglet, k) {
+                            _.forEach(onglet, function(content, prop) {
+                                if (prop !== "theme" && prop !== "id") {
+                                    center[prop] = content;
+                                    headers.push({ field: prop, enableFiltering: true });
+                                }
+                            });
+                        });
+                        data.push(center);
+                    });
+
+                    $scope.myData = data;
+                    $scope.gridOptions.data = $scope.myData;
+                });
+        }
+    ])
