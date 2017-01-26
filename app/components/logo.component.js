5fb7612f2f8d1199a3955b0528f5eac105f5ff4d
Anne L'Hôte <anne.lhote@gmail.com>
2017-01-26 13:13:04 +0100

    Display logo for "EA 7033" aka Médialab

--- app/components/logo.component.js
+++ app/components/logo.component.js
@@ -11,7 +11,7 @@ angular.module('bib.components')
     this.$onInit = function () {
       var admin = this.org.administration;
       // TODO the following organisations do not have a logo yet
-      if (['EA 7033', 'EA 4586'].indexOf(admin['Code Unité']) !== -1) return;
+      if (['EA 4586'].indexOf(admin['Code Unité']) !== -1) return;
       // example Centre Max Weber
       var src = admin['Acronyme (nom court)'] || admin['Intitulé'];
       if (src) this.src = 'img/logos_centres_de_recherche_jpeg/' + src + '.jpg';
