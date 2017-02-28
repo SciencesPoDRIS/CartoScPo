a2112db0bf6de4d6ba414f3566443ff9136fab6a
Anne L'Hôte <anne.lhote@gmail.com>
2017-02-28 16:52:39 +0100

    Add controller on view project to add anchors on the summary

---
+++ app/controllers/project.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bib.controllers')
+    .controller('project', ['$scope',
+        function($scope) {
+            $scope.scrollTo = function(elementId) {
+                $.scrollTo($('#' + elementId).offset().top - 120)
+            }
+        }
+]);
