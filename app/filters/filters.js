8d17258ed444ba0608264059f2af3a6043adeea0
Anne L'Hôte <anne.lhote@gmail.com>
2017-03-01 09:18:39 +0100

    Reindent file

--- app/filters/filters.js
+++ app/filters/filters.js
@@ -1,59 +1,57 @@
 angular.module('bib')
-.filter('visibleColumns', function($rootScope) {
-  return function(data, grid, query) {
+    .filter('visibleColumns', function($rootScope) {
+        return function(data, grid, query) {
 
-    var matches = [];
+            var matches = [];
 
-    //no filter defined so bail
-    if (query === undefined || query === '') {
-      return data;
-    }
+            //no filter defined so bail
+            if (query === undefined || query === '') {
+                return data;
+            }
 
-    query = query.toLowerCase();
+            query = query.toLowerCase();
 
-    //loop through data items and visible fields searching for match
-    var scope = $rootScope.$new(true);
-    for (var i = 0; i < data.length; i++) {
-      for (var j = 0; j < grid.columnDefs.length; j++) {
+            //loop through data items and visible fields searching for match
+            var scope = $rootScope.$new(true);
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < grid.columnDefs.length; j++) {
 
-        var dataItem = data[i];
-        var fieldName = grid.columnDefs[j].field;
-        var renderedData = dataItem[fieldName];
+                    var dataItem = data[i];
+                    var fieldName = grid.columnDefs[j].field;
+                    var renderedData = dataItem[fieldName];
 
-        // apply cell filter
-        if (grid.columnDefs[j].cellFilter) {
-          scope.value = renderedData;
-          renderedData = scope.$eval('value | ' + grid.columnDefs[j].cellFilter);
-        }
+                    // apply cell filter
+                    if (grid.columnDefs[j].cellFilter) {
+                        scope.value = renderedData;
+                        renderedData = scope.$eval('value | ' + grid.columnDefs[j].cellFilter);
+                    }
 
-        //as soon as search term is found, add to match and move to next dataItem
-        if (renderedData.toString().toLowerCase().indexOf(query) > -1) {
-          matches.push(dataItem);
-          break;
-        }
-      }
-    }
-    scope.$destroy();
+                    //as soon as search term is found, add to match and move to next dataItem
+                    if (renderedData.toString().toLowerCase().indexOf(query) > -1) {
+                        matches.push(dataItem);
+                        break;
+                    }
+                }
+            }
+            scope.$destroy();
 
-    return matches;
-  };
-})
-// the following filters can also during the csv parsing
-.filter('trimNL', function () {
-  return function trimNL (str) {
-    if (typeof str != 'string') return str;
-    return str
-      .replace(/^[\t|\r|\n]/g, '')
-      .replace(/[\t|\r|\n]$/g, '')
-      .trim();
-  };
-})
-.filter('trimStar', function () {
-  return function trimStar(str) {
-    if (typeof str != 'string') return str;
-    if (!str) return '';
-    return str.replace(/\*/, '').trim();
-  };
-})
-;
-
+            return matches;
+        };
+    })
+    // the following filters can also during the csv parsing
+    .filter('trimNL', function() {
+        return function trimNL(str) {
+            if (typeof str != 'string') return str;
+            return str
+                .replace(/^[\t|\r|\n]/g, '')
+                .replace(/[\t|\r|\n]$/g, '')
+                .trim();
+        };
+    })
+    .filter('trimStar', function() {
+        return function trimStar(str) {
+            if (typeof str != 'string') return str;
+            if (!str) return '';
+            return str.replace(/\*/g, '').trim();
+        };
+    });
