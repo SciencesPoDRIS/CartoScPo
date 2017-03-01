22e17369f85bbaf62700ac72e2f00dec3d9bc1ee
Anne L'Hôte <anne.lhote@gmail.com>
2017-03-01 17:20:33 +0100

    Toogle center on click on the center title

--- app/components/center-list.component.js
+++ app/components/center-list.component.js
@@ -1,12 +1,20 @@
 'use strict';
 
 angular.module('bib.components')
-.component('centerList', {
-  templateUrl: 'views/center-list.html',
-  bindings: {
-    centers: '<',
-    // expanded details
-    expandedCenters: '<'
-  }
-});
-
+    .component('centerList', {
+        templateUrl: 'views/center-list.html',
+        bindings: {
+            centers: '<',
+            // expanded details
+            expandedCenters: '='
+        },
+        controller: function($scope) {
+            this.toogleCenter = function(centerId) {
+                if(this.expandedCenters.indexOf(centerId) !== -1) {
+                    this.expandedCenters.splice(this.expandedCenters.indexOf(centerId), 1);
+                } else {
+                    this.expandedCenters.push(centerId);
+                }
+            }
+        }
+    });
