498bbfa015c1a4b0d6772cdff0467d1bd7a1bd2a
Delapouite <delapouite@gmail.com>
2017-12-11 14:26:39 +0100

    feat: add centers fetching from api

--- back-office/components/centers/index.js
+++ back-office/components/centers/index.js
@@ -1,11 +1,24 @@
 import angular from 'angular'
 
-class controller {}
+class controller {
+  constructor($http) {
+    this.centers = []
+    this.$http = $http
+  }
+
+  $onInit() {
+    this.$http
+      .get('/api/centers')
+      .then(({ data }) => (this.centers = data.centers))
+  }
+}
+controller.$inject = ['$http']
 
 const component = {
   template: require('./index.html'),
   controller,
 }
 
-export default angular.module('bobib.centers', []).component('centers', component).name
-
+export default angular
+  .module('bobib.centers', [])
+  .component('centers', component).name
--- server/index.js
+++ server/index.js
@@ -4,9 +4,23 @@ const express = require('express')
 const { server: config } = require('config')
 
 const PUBLIC = path.join(__dirname, '../back-office')
+const DB = path.join(__dirname, '../app/data/data.json')
+
+const db = require(DB)
+
 const app = express()
 
 app.use(express.static(PUBLIC))
+
+app.get('/api/centers', (req, res) => {
+  const centers = Array.from(Object.entries(db.allCenters)).map(
+    ([id, center]) => {
+      center.id = id
+      return center
+    },
+  )
+  res.json({ centers })
+})
 
 // single page application
 app.get('/*', (req, res) => res.sendFile(`${PUBLIC}/index.html`))
