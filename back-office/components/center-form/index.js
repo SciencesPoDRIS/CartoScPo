9c57a0dc095b183fa879e7677f00a11bef329026
Delapouite <delapouite@gmail.com>
2017-12-12 12:22:08 +0100

    feat: add rough fields and tabs for center-form component

--- back-office/components/center-form/index.js
+++ back-office/components/center-form/index.js
@@ -3,11 +3,35 @@ import logoMod from '../logo'
 
 class controller {
   constructor($http) {
-    this.center = {}
     this.$http = $http
+
+    this.center = {}
+    this.tab = 'administration'
+    this.tabs = [
+      { id: 'administration', label: 'Description' },
+      { id: 'ecole', label: 'Écoles doctorales' },
+      { id: 'recherche', label: 'Thématiques' },
+      { id: 'publications', label: 'Publications' },
+      { id: 'ressources', label: 'Documentation' },
+    ]
   }
 
   $onInit() {
+    this.$http
+      .get(`/api/centers/${this.id}`)
+      .then(({ data }) => {
+        this.center = data.center
+        this.sections = Object.keys(data.center).filter(s => s != 'id')
+      })
+      .catch(console.error)
+  }
+
+  isActive(tab) {
+    return tab.id === this.tab
+  }
+
+  setTab(tab) {
+    this.tab = tab.id
   }
 }
 controller.$inject = ['$http']
