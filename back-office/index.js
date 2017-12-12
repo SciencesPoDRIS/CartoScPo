2e133d6cd322535e4ed29cc6eda372dc37531656
Delapouite <delapouite@gmail.com>
2017-12-12 10:24:43 +0100

    feat: add ngRoute

--- back-office/index.js
+++ back-office/index.js
@@ -1,7 +1,26 @@
 import angular from 'angular'
+import ngRoute from 'angular-route'
 
 import appComponent from './components/app'
 
-angular.module('bobib', [
-  appComponent
+angular.module('bobib', [ngRoute, appComponent]).config([
+  '$locationProvider',
+  '$routeProvider',
+  ($locationProvider, $routeProvider) => {
+    $locationProvider.html5Mode(true)
+    $routeProvider
+      .when('/', {
+        template: '<h1 class="title">Home</h1>',
+      })
+      .when('/centers', {
+        template: '<centers />',
+      })
+      .when('/modifications', {
+        template: '<h1 class="title">Modifications</h1>',
+      })
+      .when('/users', {
+        template: '<h1 class="title">Users</h1>',
+      })
+      .otherwise('/')
+  },
 ])
