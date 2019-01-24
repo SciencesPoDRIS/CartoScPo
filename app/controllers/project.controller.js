cca15d6a92239c145a76ceaf4a67e6584d4fa0e2
Nicolas Chambrier <naholyr@gmail.com>
2019-01-24 10:29:31 +0100

    refactor(): renames 'project' controller into 'ProjectCtrl' for easier recognition

--- app/controllers/project.controller.js
+++ app/controllers/project.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bib.controllers').controller('project', function($scope) {
+angular.module('bib.controllers').controller('ProjectCtrl', function($scope) {
   $scope.scrollTo = function(elementId) {
     $.scrollTo($('#' + elementId).offset().top - 120);
   };
